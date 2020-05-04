94aff05a1d0a823e0fe448326cc298cb80eec338
BrandonLN <brandonln@gmail.com>
2020-05-04 15:59:22 -0600

    additional code content in model files

--- models/beer.js
+++ models/beer.js
@@ -1,12 +1,12 @@
 module.exports = function(sequelize, Sequelize) {
   // beer table -> name, description, ABV (integer), IBU (integer), type (porter,stout,ipa), foreign key (name of brewery)
   var Beer = sequelize.define('Beer', {
-    name: { type: Sequelize.String },
-    description: { type: Sequelize.String },
-    ABV: { type: Sequelize.Integer },
-    IBU: { type: Sequelize.Integer },
-    beerType: { type: Sequelize.String },
-    foreignKey: { type: Sequelize.String },
+    name: { type: Sequelize.STRING },
+    description: { type: Sequelize.STRING },
+    ABV: { type: Sequelize.INTEGER },
+    IBU: { type: Sequelize.INTEGER },
+    beerType: { type: Sequelize.STRING },
+    foreignKey: { type: Sequelize.STRING },
 
   });
 
